d3d00019f0e04cb7c7f3bcce7404f43e79dcfa18
Juan Carlos Valdés <idevkingos@gmail.com>
2021-06-07 18:30:20 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AppRoutingModule } from './app.routing.module';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PreciosComponent,
+    ProtegidaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'dev-gdo9j8z8.us.auth0.com',
+      clientId: 'R6FH87RSNuQ7DFA7fPAk4zhJDHN4nN1P',
+      cacheLocation: 'localstorage',
+      useRefreshTokens: true
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { AuthGuard } from '@auth0/auth0-angular';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
+  {
+    path: 'protegida',
+    component: ProtegidaComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
